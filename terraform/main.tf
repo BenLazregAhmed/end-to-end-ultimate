ab496b8e0b55d400b78c08ec47d3d69a69cbd68d
CRVX <benlazregahmed328@gmail.com>
2025-04-22 08:45:33 +0100

    fixing provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
-
   required_providers {
     aws = {
-      source  = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
 
   cloud {
 
@@ -11,8 +10,8 @@ terraform {
   }
   required_providers {
     aws = {
-      source  = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
