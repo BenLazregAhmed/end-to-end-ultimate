32e0aa9616e6543ad995fc5ff5404e2c4c086eb8
CRVX <benlazregahmed328@gmail.com>
2025-04-22 17:56:25 +0100

    added in terraform outputs.tf

--- terraform/modules/outputs.tf
+++ terraform/modules/outputs.tf
@@ -1,5 +1,5 @@
 output "instance_public_ip" {
-  value       = aws_instance.main_server.public_ip          # The actual value to be outputted
+  value       = aws_instance.main_server.public_ip         # The actual value to be outputted
   description = "The public IP address of the EC2 instance" # Description of what this output represents
 }
 
