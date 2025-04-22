8b45b723cc83e67972e4c83d78f13ef6816446c3
CRVX <benlazregahmed328@gmail.com>
2025-04-22 10:47:59 +0100

    allowing http inbound on port 80

--- terraform/modules/networking.tf
+++ terraform/modules/networking.tf
@@ -6,12 +6,22 @@ resource "aws_security_group" "instances" {
   name = "instance-security-group"
 }
 
-resource "aws_security_group_rule" "allow_http_inbound" {
+resource "aws_security_group_rule" "allow_http_inbound_port_8080" {
   type              = "ingress"
   security_group_id = aws_security_group.instances.id
 
   from_port   = 8080
   to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_http_inbound_port_80" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instances.id
+
+  from_port   = 80
+  to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
